79860ab681c7f4330e3bd7ab5adca8975b882190
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-04-10 00:42:52 -0600

    Delete build script

--- build.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-jekyll build
-find _site/assets/ -type f -name *.png -exec convert {} -strip {} \;
-find _site/assets/ -type f -name *.jpg -exec convert {} -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB {} \;
-s3_website push
