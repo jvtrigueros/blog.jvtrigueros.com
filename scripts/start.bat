968af71867eb6c14c09c1f5ac464641181006fe7
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-07-12 01:16:54 -0600

    Create convenient scripts for starting and stopping jekyll server

---
+++ scripts/start.bat
@@ -0,0 +1,5 @@
+@echo off
+
+docker run --rm -d --name jk -p 4000:4000 -e POLLING=true -e DRAFTS=true -v %cd%\src:/srv/jekyll jekyll/jekyll:3.5.0 jekyll s
+
+echo INFO: Starting Jekyll server on port http://localhost:4000
