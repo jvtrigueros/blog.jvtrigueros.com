39d05ff69960d8aa2849c581e35596d021c6fed2
Jose V. Trigueros <j.v.trigueros@gmail.com>
2018-12-29 16:12:26 -0800

    Update start.bat to use latest Jekyll 3.8.5

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,5 +1,5 @@
 @echo off
 
-docker run --rm -d --name jk -p 4000:4000 -e POLLING=true -e DRAFTS=true -v %cd%\src:/srv/jekyll jekyll/jekyll:3.5.0 jekyll s
+docker run --rm -d --name jk -p 4000:4000 -e FORCE_POLLING=true -e DRAFTS=true -v %cd%\src:/srv/jekyll jekyll/jekyll:3.8.5 jekyll s
 
 echo INFO: Starting Jekyll server on port http://localhost:4000
